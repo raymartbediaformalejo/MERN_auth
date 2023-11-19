27dd15c26ce35ef414b6baf2d646db90136a1f4d
Raymart <raymartformalejo00@gmail.com>
2023-11-19 23:01:24 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,6 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "dito po https://mern-auth-ashy.vercel.app",
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
