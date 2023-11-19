63e8a13cd6a284f421c5d9a8c3a250bc14a1cfd8
Raymart <raymartformalejo00@gmail.com>
2023-11-19 23:12:55 +0800

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -16,7 +16,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(statusCode).json({
     message: message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    stack: err.stack,
   });
 };
 
