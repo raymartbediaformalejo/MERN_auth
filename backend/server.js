4ed69842fdf8799e9aae9c002da9fa1decb3ba9e
Raymart <raymartformalejo00@gmail.com>
2023-11-19 20:08:36 +0800

    setup CORS

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,21 @@ import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:2000/",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
